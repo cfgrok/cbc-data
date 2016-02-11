fd97e9ba4504bf610b153138b790149a38f95a7b
Ezra Parker <ezra@cfgrok.com>
2016-02-11 00:11:22 -0800

    Add sorting for checklist observers

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -9,7 +9,7 @@ class Checklist < ActiveRecord::Base
 
   accepts_nested_attributes_for :observations, allow_destroy: true
 
-  has_and_belongs_to_many :observers
+  has_and_belongs_to_many :observers, -> { order 'last_name, first_name' }
 
   def to_s
     "#{survey.to_s}: #{area.to_s}"
--- app/models/observer.rb
+++ app/models/observer.rb
@@ -1,4 +1,6 @@
 class Observer < ActiveRecord::Base
+  scope :sorted, -> { order 'last_name, first_name' }
+
   has_and_belongs_to_many :checklists
 
   def to_s
