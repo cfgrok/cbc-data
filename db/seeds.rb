daeb853677faccbe773f6fb3f8c91302bee4b858
Ezra Parker <ezra@cfgrok.com>
2021-01-28 17:15:52 -0800

    Update automatic checklist import in seed file
       - Set end year to 120
       - Update root data directory to pCloud Sync
       - Update filename for downloaded checklist
       - Create surveys for new years
       - Change taxon name for Thayer's Gull to Iceland Gull (Thayer's)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-(100..117).each do |number|
+(100..120).each do |number|
   year = Year.create([{ audubon_year: number, vashon_year: number - 99 }])
 end
 
@@ -29,13 +29,24 @@ taxons.each_with_index do |taxon, index|
   Taxon.create([{ common_name: taxon['common_name'], cbc_name: taxon['cbc_name'], scientific_name: taxon['scientific_name'], taxonomic_order: index + 1 }])
 end
 
-DownloadedChecklistImport.new.import File.open('/home/ezra/Documents/CBC/Vashon Documents/WAVA-1997-2015.xls')
+DownloadedChecklistImport.new.import '/home/ezra/pCloud Sync/CBC/Vashon Documents/WAVA-2000-2015.xls'
 
-Survey.create([{ date: '2016-01-03', year: Year.all[-2] }])
-Survey.create([{ date: '2017-01-02', year: Year.all.last }])
+Survey.create([{ date: '2016-01-03', year: Year.all[-5] }])
+Survey.create([{ date: '2017-01-02', year: Year.all[-4] }])
+Survey.create([{ date: '2017-12-31', year: Year.all[-3] }])
+Survey.create([{ date: '2018-12-30', year: Year.all[-2] }])
+Survey.create([{ date: '2020-01-05', year: Year.all.last }])
 
-ChecklistImport.new.import_all '/home/ezra/Documents/CBC/Vashon\ 116\ -\ 01-03-2016/Data'
+ChecklistImport.new.import_all '/home/ezra/pCloud Sync/CBC/Vashon 116 - 01-03-2016/Data'
 
 Taxon.where(common_name: 'Western Scrub-Jay').update_all(common_name: 'California Scrub-Jay')
 
-ChecklistImport.new.import_all '/home/ezra/Documents/CBC/Vashon\ 117\ -\ 01-02-2017/Data'
+ChecklistImport.new.import_all '/home/ezra/pCloud Sync/CBC/Vashon 117 - 01-02-2017/Data'
+
+Taxon.where(common_name: 'Thayer\'s Gull').update_all(common_name: 'Iceland Gull (Thayer\'s)')
+
+ChecklistImport.new.import_all '/home/ezra/pCloud Sync/CBC/Vashon 118 - 12-31-2017/Data'
+
+ChecklistImport.new.import_all '/home/ezra/pCloud Sync/CBC/Vashon 119 - 12-30-2018/Data'
+
+ChecklistImport.new.import_all '/home/ezra/pCloud Sync/CBC/Vashon 120 - 01-05-2020/Data'
