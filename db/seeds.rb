09b95ec79c29e735da97c80983a77c08db7e118c
Ezra Parker <ezra@cfgrok.com>
2024-08-02 18:37:44 -0700

    feat: update seeds to import all survey years

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-(100..121).each do |number|
+(100..124).each do |number|
   year = Year.create([{ audubon_year: number, vashon_year: number - 99 }])
 end
 
@@ -29,27 +29,40 @@ taxons.each_with_index do |taxon, index|
   Taxon.create([{ common_name: taxon['common_name'], cbc_name: taxon['cbc_name'], scientific_name: taxon['scientific_name'], taxonomic_order: index + 1 }])
 end
 
-DownloadedChecklistImport.new.import '/home/ezra/pCloud Sync/CBC/Vashon Documents/WAVA-2000-2015.xls'
+DownloadedChecklistImport.new.import '/home/ezra/Sync/CBC/Vashon Documents/WAVA-2000-2015.xls'
 
-Survey.create([{ date: '2016-01-03', year: Year.all[-6] }])
-Survey.create([{ date: '2017-01-02', year: Year.all[-5] }])
-Survey.create([{ date: '2017-12-31', year: Year.all[-4] }])
-Survey.create([{ date: '2018-12-30', year: Year.all[-3] }])
-Survey.create([{ date: '2020-01-05', year: Year.all[-2] }])
-Survey.create([{ date: '2021-01-03', year: Year.all.last }])
+Survey.create([{ date: '2016-01-03', year: Year.all[-9] }])
+Survey.create([{ date: '2017-01-02', year: Year.all[-8] }])
+Survey.create([{ date: '2017-12-31', year: Year.all[-7] }])
+Survey.create([{ date: '2018-12-30', year: Year.all[-6] }])
+Survey.create([{ date: '2020-01-05', year: Year.all[-5] }])
+Survey.create([{ date: '2021-01-03', year: Year.all[-4] }])
+Survey.create([{ date: '2022-01-03', year: Year.all[-3] }])
+Survey.create([{ date: '2023-01-02', year: Year.all[-2] }])
+Survey.create([{ date: '2023-12-31', year: Year.all[-1] }])
 
-ChecklistSpreadsheet::File.new.import_all '/home/ezra/pCloud Sync/CBC/Vashon 116 - 01-03-2016/Data'
+ChecklistSpreadsheet::File.new.import_all '/home/ezra/Sync/CBC/Vashon 116 - 01-03-2016/Data'
 
 Taxon.where(common_name: 'Western Scrub-Jay').update_all(common_name: 'California Scrub-Jay')
 
-ChecklistSpreadsheet::File.new.import_all '/home/ezra/pCloud Sync/CBC/Vashon 117 - 01-02-2017/Data'
+ChecklistSpreadsheet::File.new.import_all '/home/ezra/Sync/CBC/Vashon 117 - 01-02-2017/Data'
 
 Taxon.where(common_name: 'Thayer\'s Gull').update_all(common_name: 'Iceland Gull (Thayer\'s)')
 
-ChecklistSpreadsheet::File.new.import_all '/home/ezra/pCloud Sync/CBC/Vashon 118 - 12-31-2017/Data'
+ChecklistSpreadsheet::File.new.import_all '/home/ezra/Sync/CBC/Vashon 118 - 12-31-2017/Data'
 
-ChecklistSpreadsheet::File.new.import_all '/home/ezra/pCloud Sync/CBC/Vashon 119 - 12-30-2018/Data'
+ChecklistSpreadsheet::File.new.import_all '/home/ezra/Sync/CBC/Vashon 119 - 12-30-2018/Data'
 
-ChecklistSpreadsheet::File.new.import_all '/home/ezra/pCloud Sync/CBC/Vashon 120 - 01-05-2020/Data'
+ChecklistSpreadsheet::File.new.import_all '/home/ezra/Sync/CBC/Vashon 120 - 01-05-2020/Data'
 
-ChecklistSpreadsheet::File.new.import_all '/home/ezra/pCloud Sync/CBC/Vashon 121 - 01-03-2021/Data'
+ChecklistSpreadsheet::File.new.import_all '/home/ezra/Sync/CBC/Vashon 121 - 01-03-2021/Data'
+
+Taxon.where(common_name: 'Mew Gull').update_all(common_name: 'Short-billed Gull')
+
+ChecklistSpreadsheet::File.new.import_all '/home/ezra/Sync/CBC/Vashon 122 - 01-03-2022/Data'
+
+Taxon.where(common_name: 'Bald Eagle adult').update_all(common_name: 'Bald Eagle')
+
+ChecklistSpreadsheet::File.new.import_all '/home/ezra/Sync/CBC/Vashon 123 - 01-02-2023/Data'
+
+ChecklistSpreadsheet::File.new.import_all '/home/ezra/Sync/CBC/Vashon 124 - 12-31-2023/Data'
